b02438d0c9ad6e1984268995cc91fa7993f71f70
Vladimir Ivanov <mrcitron@mail.ru>
2020-09-21 22:28:20 +0300

    add camera moving

--- src/components/Lr1.vue
+++ src/components/Lr1.vue
@@ -1,6 +1,49 @@
 <template>
   <div>
     <div>
+      <label v-for="(offset, index) in Object.keys(uniforms).filter(key => key.includes('offset'))" :key="offset+index">
+        {{ offset }}
+        <input
+            v-model="uniforms[offset].value"
+            type="range"
+            max="100"
+            min="-100"
+            step="1">
+      </label>
+    </div>
+    <div>
+      <label v-for="(color, index) in Object.keys(uniforms).filter(key => key.includes('Color'))" :key="color+index">
+        {{ color }}
+        <input
+            v-model.number="uniforms[color].value"
+            class="slider"
+            type="range"
+            max="255"
+            step="1">
+      </label>
+    </div>
+    <div>
+      <label v-for="(coord, index) in Object.keys(cameraPosition)" :key="coord+index">
+        Camera {{ coord }}
+        <input
+            v-model.number="cameraPosition[coord].value"
+            class="slider"
+            type="range"
+            :max="cameraPosition[coord].max"
+            :step="cameraPosition[coord].step">
+      </label>
+    </div>
+    <div>
+      <label>
+        Size
+        <input
+            v-model="sizeMultiply"
+            type="range"
+            max="50"
+            step="0.5">
+      </label>
+    </div>
+    <div>
       <vgl-renderer class="mainRenderer" name="mainRenderer" camera="mainCamera" scene="mainScene">
         <vgl-scene name="mainScene">
           <vgl-shader-material
@@ -35,44 +78,10 @@
           <vgl-mesh material="mat" name="textMeshW" geometry="textW" position="-30 5 0"/>
           <vgl-mesh material="mat" name="textMeshD" geometry="textD" position="20 -30 0"/>
         </vgl-scene>
-        <vgl-perspective-camera orbit-position="100 1.5 0.1" name="mainCamera"/>
+        <vgl-perspective-camera
+            :orbit-position="`${cameraPosition.x.value} ${cameraPosition.y.value} ${cameraPosition.z.value}`"
+            name="mainCamera"/>
       </vgl-renderer>
-    </div>
-    <div>
-      <table>
-        <tr v-for="(offset, index) in Object.keys(uniforms).filter(key => key.includes('offset'))" :key="index">
-          <td>{{ offset }}</td>
-          <td>
-            <input
-                v-model="uniforms[offset].value"
-                type="range"
-                max="100"
-                min="-100"
-                step="1">
-          </td>
-        </tr>
-        <tr>
-          <td>Size</td>
-          <td>
-            <input
-                v-model="sizeMultiply"
-                type="range"
-                max="50"
-                step="0.5">
-          </td>
-        </tr>
-        <tr v-for="(color, index) in Object.keys(uniforms).filter(key => key.includes('Color'))" :key="index">
-          <td>{{ color }}</td>
-            <td>
-              <input
-                  v-model.number="uniforms[color].value"
-                  class="slider"
-                  type="range"
-                  max="255"
-                  step="1">
-          </td>
-        </tr>
-      </table>
     </div>
   </div>
 </template>
@@ -93,6 +102,23 @@ export default {
   data() {
     return {
       sizeMultiply: 15,
+      cameraPosition: {
+        x: {
+          value: 100,
+          step: 0.5,
+          max: 300,
+        },
+        y: {
+          value: 1.5,
+          step: 0.05,
+          max: 3,
+        },
+        z: {
+          value: 0.1,
+          step: 0.05,
+          max: 6
+        },
+      },
       uniforms: {
         redColor: { value: 255 },
         offsetX: { value: 0.0 },
@@ -111,7 +137,6 @@ export default {
 <style scoped>
 .mainRenderer {
   height: 80vh;
-  width: 80vw;
-  float: right;
+  width: 100vw;
 }
 </style>
